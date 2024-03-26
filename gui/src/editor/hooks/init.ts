456b396fbcd4ae4287c79e0f0988a9814cceb056
Lucas de Ávila Martins <lucaxx@gmail.com>
2024-03-25 21:13:27 -0300

    GUI: Make init a tutorial (#41)

--- gui/src/editor/hooks/init.ts
+++ gui/src/editor/hooks/init.ts
@@ -1,139 +1,257 @@
 import { LmEditorState } from "../lib/types";
 
 export const initialContent: LmEditorState = {
-  version: "1",
-  variables: [
-    {
-      name: "content",
-      value: 'Question: "What is the person doing?" Answer: "The person is happy"',
-      uuid: "e3e3e3e3-e3e3-e3e3-e3e3-e3e3e3e3e3e3",
-    },
-    {
-      name: "PERSON",
-      value: "A person.",
-      uuid: "e3e3e3e3-e3e3-e3e3-e3e3-e3e3e3e3e3e4",
-    },
-    {
-      name: "OBJECT",
-      value: "An object.",
-      uuid: "e3e3e3e3-e3e3-e3e3-e3e3-e3e3e3e3e3e5",
-    },
-  ],
   doc: {
     type: "doc",
     content: [
-      { type: "authorSelect", attrs: { author: "user" } },
       {
-        type: "paragraph",
+        type: "authorSelect",
+        attrs: {
+          author: "user",
+        },
+      },
+      {
+        type: "heading",
+        attrs: {
+          level: 1,
+        },
         content: [
           {
             type: "text",
-            text: "What is the best subject for the illustration to accompany the following?",
+            text: "LmScript GUI Tutorial",
           },
         ],
-      },
-      {
-        type: "heading",
-        attrs: { level: 3 },
-        content: [{ type: "text", text: "Content" }],
-      },
-      {
-        type: "paragraph",
-        content: [
-          { type: "text", text: '"' },
-          {
-            type: "variableSelect",
-            attrs: { uuid: "e3e3e3e3-e3e3-e3e3-e3e3-e3e3e3e3e3e3" },
-          },
-          { type: "text", text: '"' },
-        ],
-      },
-      {
-        type: "heading",
-        attrs: { level: 3 },
-        content: [{ type: "text", text: "Instructions" }],
       },
       {
         type: "paragraph",
         content: [
           {
             type: "text",
-            text: "First explain why you're choosing the best subject for the illustration, then choose the best subject for the illustration, either a person or an object. Answer with just \"",
+            text: "LmScript GUI allows you to create documents that will be completed by AI.",
           },
-          {
-            type: "variableSelect",
-            attrs: { uuid: "e3e3e3e3-e3e3-e3e3-e3e3-e3e3e3e3e3e4" },
-          },
-          { type: "text", text: '" or "' },
-          {
-            type: "variableSelect",
-            attrs: { uuid: "e3e3e3e3-e3e3-e3e3-e3e3-e3e3e3e3e3e5" },
-          },
-          { type: "text", text: '".' },
         ],
-      },
-      { type: "paragraph", content: [{ type: "text", text: "For example:" }] },
-      {
-        type: "heading",
-        attrs: { level: 4 },
-        content: [{ type: "text", text: "Example 1" }],
       },
       {
         type: "paragraph",
         content: [
           {
             type: "text",
-            text: "Explanation: I'm choosing a person because the scene is about a person.",
+            text: "The editor allows you to create different formatting options, which are rendered as Markdown when sent to the AI.",
           },
         ],
-      },
-      {
-        type: "paragraph",
-        content: [
-          { type: "text", text: "Illustrate: " },
-          {
-            type: "variableSelect",
-            attrs: { uuid: "e3e3e3e3-e3e3-e3e3-e3e3-e3e3e3e3e3e4" },
-          },
-        ],
-      },
-      {
-        type: "heading",
-        attrs: { level: 4 },
-        content: [{ type: "text", text: "Example 2" }],
       },
       {
         type: "paragraph",
         content: [
           {
             type: "text",
-            text: "Explanation: I'm choosing an object because the scene is about an object.",
+            text: "You can create:",
+          },
+        ],
+      },
+      {
+        type: "heading",
+        attrs: {
+          level: 1,
+        },
+        content: [
+          {
+            type: "text",
+            text: "Heading 1",
+          },
+        ],
+      },
+      {
+        type: "heading",
+        attrs: {
+          level: 2,
+        },
+        content: [
+          {
+            type: "text",
+            text: "Heading 2",
+          },
+        ],
+      },
+      {
+        type: "heading",
+        attrs: {
+          level: 3,
+        },
+        content: [
+          {
+            type: "text",
+            text: "Heading 3",
           },
         ],
       },
       {
         type: "paragraph",
         content: [
-          { type: "text", text: "Illustrate: " },
+          {
+            type: "text",
+            text: "You can create a horizontal separator:",
+          },
+        ],
+      },
+      {
+        type: "horizontalRule",
+      },
+      {
+        type: "paragraph",
+        content: [
+          {
+            type: "text",
+            text: "Bullet list:",
+          },
+        ],
+      },
+      {
+        type: "bulletList",
+        content: [
+          {
+            type: "listItem",
+            content: [
+              {
+                type: "paragraph",
+                content: [
+                  {
+                    type: "text",
+                    text: "Item A",
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            type: "listItem",
+            content: [
+              {
+                type: "paragraph",
+                content: [
+                  {
+                    type: "text",
+                    text: "Item B",
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      },
+      {
+        type: "paragraph",
+        content: [
+          {
+            type: "text",
+            text: "And also a numbered list",
+          },
+        ],
+      },
+      {
+        type: "orderedList",
+        attrs: {
+          start: 1,
+        },
+        content: [
+          {
+            type: "listItem",
+            content: [
+              {
+                type: "paragraph",
+                content: [
+                  {
+                    type: "text",
+                    text: "Item A",
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            type: "listItem",
+            content: [
+              {
+                type: "paragraph",
+                content: [
+                  {
+                    type: "text",
+                    text: "Item B",
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      },
+      {
+        type: "paragraph",
+        content: [
+          {
+            type: "text",
+            text: "Besides the regular rich text editing, there are other features.",
+          },
+        ],
+      },
+      {
+        type: "paragraph",
+        content: [
+          {
+            type: "text",
+            text: "You can use a variable. Make sure to configure it on the right-side-bar.",
+          },
+        ],
+      },
+      {
+        type: "paragraph",
+        content: [
           {
             type: "variableSelect",
-            attrs: { uuid: "e3e3e3e3-e3e3-e3e3-e3e3-e3e3e3e3e3e5" },
+            attrs: {
+              uuid: "98776deb-b048-4e90-96ff-90642c207c02",
+            },
           },
         ],
       },
-      { type: "authorSelect", attrs: { author: "assistant" } },
       {
         type: "paragraph",
         content: [
-          { type: "text", text: "Explanation: " },
+          {
+            type: "text",
+            text: "Before we get to the AI parts, let's change the current message author.",
+          },
+        ],
+      },
+      {
+        type: "authorSelect",
+        attrs: {
+          author: "assistant",
+        },
+      },
+      {
+        type: "paragraph",
+        content: [
+          {
+            type: "text",
+            text: "We can use AI to generate content, ending on a new line, or any stop pattern we'd like:",
+          },
+        ],
+      },
+      {
+        type: "paragraph",
+        content: [
+          {
+            type: "text",
+            text: "We can start the line and ",
+          },
           {
             type: "lmGenerator",
             attrs: {
-              id: "720ddbc0-12e6-4583-83b6-d0229a60445b",
+              id: "be851f4c-8c63-49bc-a303-8078470308c2",
               choices: [],
               type: "generation",
-              max_tokens: 256,
-              name: "_explanation",
+              max_tokens: 16,
+              name: "generation2",
               stop: ["\n"],
             },
           },
@@ -142,32 +260,65 @@ export const initialContent: LmEditorState = {
       {
         type: "paragraph",
         content: [
-          { type: "text", text: "Illustrate: " },
+          {
+            type: "text",
+            text: "And we can choose among a set of options:",
+          },
+        ],
+      },
+      {
+        type: "paragraph",
+        content: [
+          {
+            type: "text",
+            text: '"The best programming language is ',
+          },
           {
             type: "lmGenerator",
             attrs: {
-              id: "id9100e156-5a1c-4ae5-bcf9-bc531a659220",
+              id: "df9a7dec-146f-4857-b376-45573488ef81",
               choices: [
                 {
-                  label: "{PERSON}",
-                  value: "e3e3e3e3-e3e3-e3e3-e3e3-e3e3e3e3e3e4",
-                  tag: "variable",
+                  tag: "typed",
+                  value: "javascript",
                 },
                 {
-                  label: "{OBJECT}",
-                  value: "e3e3e3e3-e3e3-e3e3-e3e3-e3e3e3e3e3e5",
-                  tag: "variable",
+                  tag: "typed",
+                  value: "typescript",
                 },
               ],
               type: "selection",
               max_tokens: 16,
-              name: "illustrator",
+              name: "select1",
               stop: [],
             },
+          },
+          {
+            type: "text",
+            text: '"',
+          },
+        ],
+      },
+      {
+        type: "paragraph",
+        content: [
+          {
+            type: "text",
+            text: "Remember: You can type / to open the suggestion menu, or click on the icons to the left of the paragraph!",
           },
         ],
       },
     ],
   },
-  samplingParams: { temperature: 0.1 },
+  variables: [
+    {
+      name: "variable1",
+      value: "The content of the first variable.",
+      uuid: "98776deb-b048-4e90-96ff-90642c207c02",
+    },
+  ],
+  samplingParams: {
+    temperature: 0.1,
+  },
+  version: "1",
 };
